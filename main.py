04a1b3efff34b2e043a205eeb87904d955a6991d
Abhijith1993 <64603617+Abhijith1993@users.noreply.github.com>
2020-05-25 16:39:05 +0300

    add title icon and bgc

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ pygame.init()
 
 # creat thae scren
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("ship game")
+# Titles and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('rocket.png' )
+pygame.display.set_icon(icon)
 
-#Game Loop
+#  Game Loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # RGB - red green blue
+    screen.fill((0, 0, 0)) # this code is used to screen colour
+    pygame.display.update()  #this code is used to update screen
