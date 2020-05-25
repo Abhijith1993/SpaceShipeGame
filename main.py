592eeb02ef2174b55cb7198a1735f2ffc8b9b339
Abhijith1993 <64603617+Abhijith1993@users.noreply.github.com>
2020-05-25 19:39:04 +0300

    enemy movement add

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ playerX_change = 0
 enemyimg = pygame.image.load('e1.png')
 enemyX = random.randint(0, 800)
 enemyY = random.randint(50, 150)
-enemyX_change = 0
+enemyX_change = 0.3
+enemyY_change = 40
 
 
 def player(X, Y):
@@ -53,12 +54,25 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+
+  # space ship movement
     playerX += playerX_change
 
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
+
+
+  # enemy movement
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
